92f9db212b7849db94b2ccaf73f1d3f8b50cbe47
ababyduck <kassisaf@gmail.com>
2017-09-22 16:58:30 -0700

    Added pop logic for Baronial Bat (untested)

---
+++ scripts/zones/Gustav_Tunnel/mobs/Baronial_Bat.lua
@@ -0,0 +1,14 @@
+----------------------------------
+-- Area: Gustav Tunnel
+--  MOB: Baronial Bat
+-----------------------------------
+package.loaded["scripts/zones/Gustav_Tunnel/TextIDs"] = nil;
+-----------------------------------
+require("scripts/zones/Gustav_Tunnel/TextIDs");
+
+-----------------------------------
+-- onMobDeath
+-----------------------------------
+function onMobDeath(mob, player, isKiller)
+    --player:setVar("WSNMKILLED_EVISC",1);
+end;
